dc12407fae379303025bf7632f59a0891b9b8563
Gregory Ashton <gregory.ashton@ligo.org>
2018-10-04 15:56:18 +1000

    Simplify script

--- run_from_cache.py
+++ run_from_cache.py
@@ -5,21 +5,19 @@ from __future__ import division, print_function
 import numpy as np
 import logging
 import sys
-# import os
-# import deepdish
 import argparse
 from glob import glob
-# import tupak
-from tupak import run_sampler
-from tupak.core.utils import setup_logger, command_line_parser
-from tupak.core.prior import Uniform
-from tupak.gw.waveform_generator import WaveformGenerator
-from tupak.gw.source import lal_binary_black_hole
-from tupak.gw.conversion import (
+
+from bilby import run_sampler
+from bilby.core.utils import setup_logger, command_line_parser
+from bilby.core.prior import Uniform
+from bilby.gw.waveform_generator import WaveformGenerator
+from bilby.gw.source import lal_binary_black_hole
+from bilby.gw.conversion import (
     convert_to_lal_binary_black_hole_parameters, generate_all_bbh_parameters)
-from tupak.gw.detector import InterferometerList
-from tupak.gw.prior import BBHPriorSet
-from tupak.gw.likelihood import GravitationalWaveTransient
+from bilby.gw.detector import InterferometerList
+from bilby.gw.prior import BBHPriorSet
+from bilby.gw.likelihood import GravitationalWaveTransient
 from bilby_pipe.utils import load_data_from_cache_file
 
 
@@ -29,28 +27,18 @@ parser.add_argument("-p", "--prior_file",
                     default=None, help="prior file")
 parser.add_argument("--trigger_time", type=float, help="Trigger time")
 parser.add_argument("--frame_caches", default=None, nargs="*",
-                    help="Cache files containing information about frames.\n"
-                    "If not provided '*.lcf' and '*.gwf' will be globbed.")
+                    help="Cache files containing information about frames")
 parser.add_argument("--channel_names", default=None, nargs="*",
                     help="Channel names to use, if not provided known "
                     "channel names will be tested.")
 parser.add_argument('--psd_duration', default=500, type=int,
                     help='Time used to generate the PSD, default is 500.')
-parser.add_argument('--dist-marg', dest='distance', action='store_true')
-parser.add_argument('--no-dist-marg', dest='distance', action='store_false')
-parser.add_argument('--phase-marg', dest='phase', action='store_true')
-parser.add_argument('--no-phase-marg', dest='phase', action='store_false')
-parser.add_argument('--time-marg', dest='time', action='store_true')
-parser.add_argument('--no-time-marg', dest='time', action='store_false')
-parser.set_defaults(time=True, phase=True, distance=False)
+parser.add_argument('--dist-marg', dest='distance', action='store_true', default=False)
+parser.add_argument('--phase-marg', dest='phase', action='store_true', default=True)
+parser.add_argument('--time-marg', dest='time', action='store_true', default=True)
 parser.add_argument('--outdir', default='outdir', help='Output directory')
 args = parser.parse_args()
 
-# if len(args.interferometers) == 1:
-#     if ' ' in args.interferometers[0]:
-#         args.interferometers = args.interferometers[0].split(" ")
-# idx = args.idx
-# injection_file = args.injection_file
 prior_file = args.prior_file
 
 setup_logger(log_level="info")
@@ -86,38 +74,6 @@ if args.frame_caches is not None:
         ifos.append(load_data_from_cache_file(
             cache_file, args.trigger_time, duration, args.psd_duration,
             channel_name))
-elif glob('./*.lcf') > 0:
-    cache_files = glob('./*.lcf')
-    if args.channel_names is None:
-        args.channel_names = [None]*len(cache_files)
-    for cache_file, channel_name in zip(cache_files, args.channel_names):
-        ifos.append(load_data_from_cache_file(
-            cache_file, args.trigger_time, duration, args.psd_duration,
-            channel_name))
-elif glob('./*.gwf') > 0:
-    # FIXME
-    # for ifo in ifos:
-    #     ifo.strain_data.roll_off = 0.0
-    #     ifo_frame_files = [ff for ff in frames if ifo.name in ff]
-    #     ifo_frame_file = [ff for ff in ifo_frame_files
-    #                       if '{}_{}_'.format(ifo.name, frame_frame_start)
-    #                       in ff][0]
-    #     ifo.strain_data.set_from_frame_file(
-    #         ifo_frame_file, sampling_frequency=sampling_frequency,
-    #         duration=duration, frame_start=epoch,
-    #         channel='{}:TBS_MDC'.format(ifo.name), buffer_time=0)
-    #     if args.psd_duration > 0:
-    #         if epoch - args.psd_duration > frame_frame_start:
-    #             psd_frame_start = epoch - args.psd_duration
-    #         else:
-    #             psd_frame_start = epoch + duration
-    #         ifo.power_spectral_density.set_from_frame_file(
-    #             ifo_frame_file, psd_frame_start, args.psd_duration,
-    #             fft_length=duration,
-    #             sampling_frequency=sampling_frequency,
-    #             roll_off=ifo.strain_data.roll_off,
-    #             channel='{}:TBS_MDC'.format(ifo.name))
-    raise Exception("This doesn't work.")
 else:
     raise Exception("Cannot find any data!")
 
