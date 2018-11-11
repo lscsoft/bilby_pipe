172e76ee151b3f1c34f509e26dea221a97e48945
Gregory Ashton <gregory.ashton@ligo.org>
2018-11-12 09:51:26 +1100

    Add basic documentation

--- bilby_pipe/__init__.py
+++ bilby_pipe/__init__.py
@@ -1,3 +1,12 @@
+"""
+bilby_pipe
+==========
+
+bilby_pipe is a python3 tool for automating the process of running `bilby
+<https://git.ligo.org/lscsoft/bilby>`_ for transient gravitational parameter
+estimation on computing clusters.
+
+"""
 from .main import Input  # noqa
 from .main import Dag  # noqa
 from .main import parse_args  # noqa
