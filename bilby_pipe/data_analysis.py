995275054a68ed8bf42367f981e574274499cbba
Isobel Romero-Shaw <isobel.romeroshaw@gmail.com>
2018-11-29 11:30:12 +1100

    updating BBHPriorSet to BBHPriorDict

--- bilby_pipe/data_analysis.py
+++ bilby_pipe/data_analysis.py
@@ -19,7 +19,7 @@ from bilby_pipe.main import Input, DataDump, parse_args
 def create_parser():
     """ Generate a parser for the data_analysis.py script
 
-    Additional options can be added to the returned parser beforing calling
+    Additional options can be added to the returned parser before calling
     `parser.parse_args` to generate the arguments`
 
     Returns
@@ -165,7 +165,7 @@ class DataAnalysisInput(Input):
 
     @property
     def priors(self):
-        priors = bilby.gw.prior.BBHPriorSet(
+        priors = bilby.gw.prior.BBHPriorDict(
             filename=self.prior_file)
         priors['geocent_time'] = bilby.core.prior.Uniform(
             minimum=self.trigger_time - self.deltaT / 2,
