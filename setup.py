23800c072402dc796d30d7aa7b1683f4973ee65e
Gregory Ashton <gregory.ashton@ligo.org>
2019-06-18 06:44:13 -0500

    Version 0.2.1 release

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def get_long_description():
     return long_description
 
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 version_file = write_version_file(VERSION)
 long_description = get_long_description()
 
