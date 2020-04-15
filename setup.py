e054d49e242659ac50a891825bfed79f8106e300
Isobel Marguarethe Romero-Shaw <isobel.romero-shaw@ldas-pcdev5.ligo.caltech.edu>
2020-04-14 18:30:29 -0700

    Version 0.3.11 release

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ def get_long_description():
     return long_description
 
 
-VERSION = "0.3.10"
+VERSION = "0.3.11"
 version_file = write_version_file(VERSION)
 long_description = get_long_description()
 
